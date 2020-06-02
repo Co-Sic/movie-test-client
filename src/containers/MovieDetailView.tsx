8272a29cdde4a5d10562e94f8fda2ebfad88d7dd
sic <patrick-kl@web.de>
2020-06-02 16:38:57 +0200

    Add responsive buttons to detail view

--- src/containers/MovieDetailView.tsx
+++ src/containers/MovieDetailView.tsx
@@ -9,6 +9,7 @@ import {GET_MOVIE_ALREADY_RATED, GET_MOVIE_BY_ID} from "../api/queries";
 import RatingsView from "./RatingsView";
 import AverageRating from "../components/AverageRating";
 import {DeleteMovieButton, EditMovieButton, RateMovieButton} from "../components/actions";
+import ArrowBackIcon from "@material-ui/icons/ArrowBack";
 
 interface MovieDetailViewProps {
     movie: Movie;
@@ -34,7 +35,7 @@ function MovieDetailView(props: MovieDetailViewProps) {
     if (error || movieError) {
         return <p>ERROR</p>
     }
-    if (!data ||!movieData) return <p>Not found</p>;
+    if (!data || !movieData) return <p>Not found</p>;
     let alreadyRated = data.alreadyRated;
     let movie = movieData.movie;
     const formattedDate = new Date(movie.releaseDate).toLocaleDateString("default", {
@@ -53,44 +54,65 @@ function MovieDetailView(props: MovieDetailViewProps) {
 
                 </TitleWrapper>
                 <TitleActionsDiv>
-                    <RateMovieButton onStartRating={() => props.onStartRating(movie)} alreadyRated={alreadyRated}/>
-                    <EditMovieButton onEdit={() => props.onEditMovie(movie)}/>
-                    <DeleteMovieButton onDelete={() => props.onDeleteMovie(movie)}/>
-                    <IconButton
-                        size={"small"}
-                        className={"table-action-button"}
-                        onClick={props.onDialogClose}
-                    >
-                        <CloseIcon/>
-                    </IconButton>
+                    <MobileWrapper>
+                        <IconButton
+                            size={"small"}
+                            className={"table-action-button"}
+                            onClick={props.onDialogClose}
+                        >
+                            <ArrowBackIcon/>
+                        </IconButton>
+                    </MobileWrapper>
+                    <NonMobileWrapper>
+                        <RateMovieButton onStartRating={() => props.onStartRating(movie)} alreadyRated={alreadyRated}/>
+                        <EditMovieButton onEdit={() => props.onEditMovie(movie)}/>
+                        <DeleteMovieButton onDelete={() => props.onDeleteMovie(movie)}/>
+                        <IconButton
+                            size={"small"}
+                            className={"table-action-button"}
+                            onClick={props.onDialogClose}
+                        >
+                            <CloseIcon/>
+                        </IconButton>
+                    </NonMobileWrapper>
+
                 </TitleActionsDiv>
             </TitleDiv>
             <DetailDiv>
-                <LineWrapper>
-                    <StyledLabel>{"Average Rating: "}</StyledLabel>
-                    <AverageRating averageRating={movie.averageRating}/>
-                    <StyledUserCount>
-                        {"(" + movie.ratingCount + ")"}
-                    </StyledUserCount>
-                </LineWrapper>
-                <LineWrapper>
-                    <StyledLabel>{"Release Date:"}</StyledLabel>
-                    {formattedDate}
-                </LineWrapper>
-                <LineWrapper>
-                    <StyledLabel>{"Duration:"}</StyledLabel>
-                    {formatDuration(movie.durationSeconds)}
-                </LineWrapper>
-                <LineWrapper>
-                    <StyledLabel>{"Actors: "}</StyledLabel>
-                    {movie.actors.map((actor: Actor, index: number) => {
-                        if (index < movie.actors.length - 1) {
-                            return actor.name + ", "
-                        }
-                        return actor.name;
-                    })}
-                </LineWrapper>
-                <RatingsView movie={movie}/>
+                <DetailContentWrapper>
+                    <LineWrapper>
+                        <StyledLabel>{"Average Rating: "}</StyledLabel>
+                        <AverageRating averageRating={movie.averageRating}/>
+                        <StyledUserCount>
+                            {"(" + movie.ratingCount + ")"}
+                        </StyledUserCount>
+                    </LineWrapper>
+                    <LineWrapper>
+                        <StyledLabel>{"Release Date:"}</StyledLabel>
+                        {formattedDate}
+                    </LineWrapper>
+                    <LineWrapper>
+                        <StyledLabel>{"Duration:"}</StyledLabel>
+                        {formatDuration(movie.durationSeconds)}
+                    </LineWrapper>
+                    <LineWrapper>
+                        <StyledLabel>{"Actors: "}</StyledLabel>
+                        {movie.actors.map((actor: Actor, index: number) => {
+                            if (index < movie.actors.length - 1) {
+                                return actor.name + ", "
+                            }
+                            return actor.name;
+                        })}
+                    </LineWrapper>
+                    <RatingsView movie={movie}/>
+                </DetailContentWrapper>
+
+                <VerticalSpacer />
+                <MobileActionsBottom>
+                    <RateMovieButton label={"Rate"} onStartRating={() => props.onStartRating(movie)} alreadyRated={alreadyRated}/>
+                    <EditMovieButton label={"Edit"} onEdit={() => props.onEditMovie(movie)}/>
+                    <DeleteMovieButton label={"Delete"} onDelete={() => props.onDeleteMovie(movie)}/>
+                </MobileActionsBottom>
             </DetailDiv>
         </RootDiv>
     );
@@ -113,6 +135,7 @@ const RootDiv = styled("div")`
     display: flex;
     flex-direction: column;
     height: 100%;
+    overflow: hidden; 
 `;
 
 const TitleDiv = styled("div")`
@@ -136,15 +159,25 @@ const TitleActionsDiv = styled("div")`
     flex-wrap: nowrap;
     flex-direction: row;
     @media(max-width: ${p => p.theme.breakpoints.values.sm}px) {
-        align-self: flex-end;
+        align-self: flex-start;
         margin-bottom: 4px;
     }
     
 `;
 
 const DetailDiv = styled("div")`
+    display: flex;
+    flex-direction: column;
     border-top: 1px solid ${p => p.theme.palette.border.default};
+    flex-grow: 1;
+    overflow: auto; 
+`;
+
+const DetailContentWrapper = styled("div")`
+    display: flex;
+    flex-direction: column;
     padding: 20px;
+    overflow: auto; 
 `;
 
 const LineWrapper = styled("div")`
@@ -152,6 +185,35 @@ const LineWrapper = styled("div")`
     flex-direction: row;
     align-items: center;
     padding-bottom: 16px;
+`;
+
+const MobileWrapper = styled("div")`
+    display: flex;
+    flex-direction: row;
+    @media(min-width: ${p => p.theme.breakpoints.values.sm}px) {
+        display: none;
+    }
+`;
+const NonMobileWrapper = styled("div")`
+    display: flex;
+    flex-direction: row;
+    @media(max-width: ${p => p.theme.breakpoints.values.sm}px) {
+        display: none;
+    }
+`;
+
+const VerticalSpacer = styled("div")`
+    flex-grow: 1;
+`;
+
+const MobileActionsBottom = styled("div")`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    border-top: 1px solid ${p => p.theme.palette.border.default};
+    @media(min-width: ${p => p.theme.breakpoints.values.sm}px) {
+        display: none;
+    }
 `;
 
 export default MovieDetailView;
